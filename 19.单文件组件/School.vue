d40b915cf2e4ab1db2b5231908561df084cbcde8
yyc <1536376956@qq.com>
2022-10-19 16:14:44 +0800

    master

---
+++ 19.单文件组件/School.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 组件标签 -->
+  <div class="demo">
+    <h2>学校名称：{{ schoolName }}</h2>
+    <h2>学校地址：{{ address }}</h2>
+    <button @click="showName">点我提示学校名</button>
+  </div>
+</template>
+
+<script>
+// 组件交互相关
+export default {
+  name: "School",
+  data() {
+    return {
+      schoolName: "尚硅谷",
+      address: "北京",
+    };
+  },
+  methods: {
+    showName() {
+      alert(this.schoolName);
+    },
+  },
+};
+</script>
+
+<style>
+/* 组件样式 */
+.demo {
+  background-color: orange;
+}
+</style>
---
+++ js/readme.txt
@@ -0,0 +1,5 @@
+dayjs.min.js 时间格式
+
+vue.js vue框架开发板
+
+vue.min.js vue框架生产版
